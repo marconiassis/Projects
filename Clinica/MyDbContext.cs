9047b1b1df5ff89d1b79a03177d7cee86a9eec2f
Marconi <55356877+marconiassis@users.noreply.github.com>
2023-10-15 05:57:01 -0300

    sync
    sync

--- Clinica/MyDbContext.cs
+++ Clinica/MyDbContext.cs
@@ -20,8 +20,8 @@ public partial class MyDbContext : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseMySql("server=192.168.56.101;user id=web;password=;database=DADOS_CLINICA", Microsoft.EntityFrameworkCore.ServerVersion.Parse("8.0.33-mysql"));
-
+        => optionsBuilder.UseMySql("server=192.168.56.101;user id=web;password=CXtz4ff#;database=DADOS_CLINICA", Microsoft.EntityFrameworkCore.ServerVersion.Parse("8.0.33-mysql"));
+   
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder
